1e33c9a7a2af7499155b1f8c605f5fa079db242b
rafabruno <brunor_morais@hotmail.com>
2020-11-21 07:18:28 -0300

    added avenir font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 
-import dinWoff2 from '../assets/fonts/DINCondensed-Bold.woff2'
-import dinWoff from '../assets/fonts/DINCondensed-Bold.woff'
+import dinBold from '../assets/fonts/DINCondensed-Bold.ttf'
+import avenirBlack from '../assets/fonts/Avenir-Black.ttf'
+import avenirMedium from '../assets/fonts/Avenir-Medium.ttf'
+import avenirHeavy from '../assets/fonts/Avenir-Heavy.ttf'
+import avenirRomam from '../assets/fonts/Avenir-Roman.ttf'
 
 export default createGlobalStyle`
   *, *::before, *::after {
@@ -9,9 +12,41 @@ export default createGlobalStyle`
   }
 
   @font-face {
+    font-family: 'Avenir';
+    src: url(${avenirRomam}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Avenir';
+    src: url(${avenirMedium}) format('truetype');
+    font-weight: 500;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Avenir';
+    src: url(${avenirBlack}) format('truetype');
+    font-weight: 900;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
+    font-family: 'Avenir Heavy';
+    src: url(${avenirHeavy}) format('truetype');
+    font-weight: 900;
+    font-style: normal;
+    font-display: swap;
+  }
+
+  @font-face {
     font-family: 'DIN Condensed';
-    src: url(${dinWoff2}) format('woff2'), url(${dinWoff}) format('woff');
-    font-weight: bold;
+    src: url(${dinBold}) format('truetype');
+    font-weight: 700;
     font-style: normal;
     font-display: swap;
   }
@@ -21,6 +56,6 @@ export default createGlobalStyle`
   }
 
   body, input, button {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
   }
 `
