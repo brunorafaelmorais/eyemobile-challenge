1903dd03651114f2dd26d98a87bafa521ece9d7b
rafabruno <brunor_morais@hotmail.com>
2020-11-20 22:25:23 -0300

    added fontface DIN Condensed

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,19 @@
 import { createGlobalStyle } from 'styled-components'
 
+import dinWoff2 from '../assets/fonts/DINCondensed-Bold.woff2'
+import dinWoff from '../assets/fonts/DINCondensed-Bold.woff'
+
 export default createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: 'DIN Condensed';
+    src: url(${dinWoff2}) format('woff2'), url(${dinWoff}) format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
   }
 
   body {
